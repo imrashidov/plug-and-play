8951e5c7bfdaf4c5877b002e03520a50226229fc
Farid Rashidov <frashidov@gmail.com>
2024-04-08 16:50:16 +0400

    Add: BestSellingProducts component with favorite functionality and data

--- src/components/BestSellingProducts.jsx
+++ src/components/BestSellingProducts.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import BestSellingProductsData from "../data/BestSellingProductsData";
 import { Link } from "react-router-dom";
+import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavorite } from "react-icons/md";
 
 const BestSellingProducts = () => {
+  const [favorite, setFavorite] = useState([1, 4]);
+  const handleFavoriteChange = (id) => {
+    if (favorite.includes(id)) {
+      setFavorite(favorite.filter((favId) => favId !== id));
+    } else {
+      setFavorite([...favorite, id]);
+    }
+  };
+
   return (
     <section id="best-selling-products">
       <div className="container">
-        <div className="bsp-products-main">
+        <div className="bsp-products-main ">
           <h2 className="section-title">Best Selling Products</h2>
           <div className="bsp-products">
             {BestSellingProductsData.map((product) => (
-              <div key={product.id} className="group">
-                <div className="bsp-product group">
+              <div key={product.id} className="bsp-product-main group">
+                <button onClick={() => handleFavoriteChange(product.id)}>
+                  {favorite.includes(product.id) ? (
+                    <MdFavorite className="favorite-icon" />
+                  ) : (
+                    <MdFavoriteBorder className="favorite-icon" />
+                  )}
+                </button>
+                <div className="bsp-product">
                   <img src={product.image} alt={product.name} />
                 </div>
                 <div className="bsp-product__info">
--- src/data/BestSellingProductsData.jsx
+++ src/data/BestSellingProductsData.jsx
@@ -7,16 +7,41 @@ import bsp6 from "../assets/BestSellingProducts/bsp6.png";
 import bsp7 from "../assets/BestSellingProducts/bsp7.webp";
 
 const BestSellingProductsData = [
-  { id: 1, image: bsp1, name: "Product 1", price: 100 },
-  { id: 2, image: bsp2, name: "Product 2", price: 200 },
-  { id: 3, image: bsp3, name: "Product 3", price: 300 },
-  { id: 4, image: bsp4, name: "Product 4", price: 400 },
-  { id: 5, image: bsp5, name: "BenQ ZOWIE XL2566K", price: 549 },
-  { id: 6, image: bsp6, name: "Product 6", price: 600 },
+  {
+    id: 1,
+    image: bsp1,
+    name: "Logitech G Pro X Superlight Wireless",
+    price: 100,
+  },
+  {
+    id: 2,
+    image: bsp2,
+    name: "Xbox Elite Wireless Controller Series 2 – Core",
+    price: 200,
+  },
+  {
+    id: 3,
+    image: bsp3,
+    name: "HyperX QuadCast S - Gaming Microphone",
+    price: 300,
+  },
+  {
+    id: 4,
+    image: bsp4,
+    name: "SteelSeries Rival 5 - Gaming Mouse",
+    price: 400,
+  },
+  {
+    id: 5,
+    image: bsp5,
+    name: "BenQ ZOWIE XL2566K 360 Hz",
+    price: 549,
+  },
+  { id: 6, image: bsp6, name: "WD Black 5TB P10 Game Drive 2.5", price: 600 },
   {
     id: 7,
     image: bsp7,
-    name: "ASUS ROG RTX 4090 ",
+    name: "ASUS ROG RTX 4090 Strix Gaming Graphics Card",
     price: 700,
   },
 ];
