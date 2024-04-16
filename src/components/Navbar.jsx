b1c274bf008bea8e457426683f906c6d39b74caf
Farid Rashidov <frashidov@gmail.com>
2024-04-16 07:28:44 +0400

    Update: Navbar component to include dynamic page links and search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import PagesData from "../data/PagesData";
 import { FiSearch } from "react-icons/fi";
 import { FiShoppingCart } from "react-icons/fi";
 import logo from "../assets/logo.png";
@@ -12,14 +13,23 @@ const Navbar = () => {
             <img src={logo} alt="logo" />
           </Link>
           <ul>
-            <Link>Home</Link>
-            <Link>About</Link>
-            <Link>Contact</Link>
+            {PagesData.map((page) => (
+              <li key={page.id}>
+                <Link to={page.path}>{page.title}</Link>
+              </li>
+            ))}
           </ul>
           <div className="nav-tools">
-            <button href="/">
-              <FiSearch />
-            </button>
+            <div className="flex relative ">
+              <input
+                type="text"
+                placeholder="What are you looking for ?"
+                className="p-3 text-center outline-none bg-transparent border-2 rounded-2xl border-neutral-600 text-neutral-400 text-base placeholder:text-xs placeholder:text-neutral-300 placeholder:text-left focus:placeholder:text-transparent"
+              />
+              <i className="absolute h-full right-0 p-2 flex items-center  ">
+                <FiSearch className="stroke-neutral-600 text-xl" />
+              </i>
+            </div>
             <Link href="/">
               <FiShoppingCart />
             </Link>
---
+++ src/data/PagesData.jsx
@@ -0,0 +1,19 @@
+const PagesData = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    title: "Contact",
+    path: "/contact",
+  },
+];
+
+export default PagesData;
