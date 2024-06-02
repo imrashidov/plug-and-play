74c85bea519516c49d5fc05d1dab3b884154a86d
Farid Rashidov <frashidov@gmail.com>
2024-06-02 09:39:34 +0400

    Add: Header mobile component and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,52 @@
-import HeaderDesktop from "./HeaderDesktop";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import pagesData from "~/data/PagesData";
+import { FiSearch } from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
+import { IoChevronDown } from "react-icons/io5";
+import logo from "~/assets/logo.png";
+import HeaderMobile from "./HeaderMobile";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [dropDownActive, setDropDownActive] = useState(false);
+
+  const toggleDropDown = () => {
+    setDropDownActive(!dropDownActive);
+  };
+
   return (
-    <nav id="navbar">
+    <header id="navbar">
       <div className="nav-container">
-        <HeaderDesktop />
+        <div className="nav-main">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <ul className="nav-links">
+            {pagesData.map((page) => (
+              <li key={page.id}>
+                <Link to={page.path}>{page.title}</Link>
+              </li>
+            ))}
+          </ul>
+          <div className="nav-tools">
+            <div className="nav-search">
+              <input type="text" placeholder="What are you looking for ?" />
+              <button>
+                <FiSearch />
+              </button>
+            </div>
+            <Link to="/">
+              <FiShoppingCart />
+            </Link>
+            <Link className="nav-sign" to="/login">
+              Sign in
+            </Link>
+          </div>
+          <HeaderMobile pagesData={pagesData} />
+        </div>
       </div>
-    </nav>
+    </header>
   );
 };
 
--- src/components/Header/HeaderMobile.jsx
+++ src/components/Header/HeaderMobile.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
+
 const HeaderMobile = () => {
-  return <div>HeaderMobile</div>;
+  return (
+    <div className="nav-mobile-main">
+      <Link to="/">
+        <FiShoppingCart />
+      </Link>
+      <Link className="nav-sign" to="/login">
+        Sign in
+      </Link>
+    </div>
+  );
 };
 
 export default HeaderMobile;
