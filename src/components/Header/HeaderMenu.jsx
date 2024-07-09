d72de30e33b117925f10b6494b12f82cf922c2fb
Farid Rashidov <frashidov@gmail.com>
2024-07-09 16:32:05 +0400

    Test

--- src/components/Header/HeaderMenu.jsx
+++ src/components/Header/HeaderMenu.jsx
@@ -5,7 +5,6 @@ import { MdDarkMode, MdLightMode } from "react-icons/md";
 import { HiLogout } from "react-icons/hi";
 import { FaUser, FaShoppingBasket } from "react-icons/fa";
 import SearchModal from "~/components/Modals/SearchModal";
-import profilePicture from "~/assets/profile-picture.jpg";
 
 const dropdownContent = {
   open: { opacity: 1, display: "block" },
@@ -19,6 +18,7 @@ const HeaderMenu = ({
   toggleDarkMode,
   searchModal,
 }) => {
+  console.log(isOpen);
   return (
     <>
       {searchModal && <SearchModal />}
